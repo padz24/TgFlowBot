0a443d724b35e2e7312b169998613211529693f3
padz24 <padz24@github.com>
2026-07-28 20:39:43 +0700

    Add missing HashSet import

--- app/src/main/java/com/tgflowbot/view/FlowCanvasView.java
+++ app/src/main/java/com/tgflowbot/view/FlowCanvasView.java
@@ -24,6 +24,7 @@ import com.tgflowbot.model.NodeType;
 import com.tgflowbot.model.Workflow;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
